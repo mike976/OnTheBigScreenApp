e68c315dfa82dc25a00d1ecdd7756845587e3ba0
Michael Bullock <michaelbullock@Michaels-Mac.local>
2020-05-03 18:03:53 +0100

    collection controller now displays movie and tv shows

--- TheBigScreenDb/Controllers/MoviesAndTVShowsViewController.swift
+++ TheBigScreenDb/Controllers/MoviesAndTVShowsViewController.swift
@@ -13,88 +13,91 @@ private let reuseIdentifier = "VideoCell"
 class MoviesAndTVShowsViewController: UIViewController {
 
        
+    
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     var collectionViewLayout: UICollectionViewFlowLayout!
     
-    var movies: [Movie]? {
-        didSet {
-            collectionView?.reloadData()
-        }
-    }
-    var tvShows: [TvShow]? {
-        didSet {
-            collectionView?.reloadData()
-        }
-    }
+    var movies: [Movie]? //{
+////        didSet {
+////            collectionView?.reloadData()
+////        }
+//    }
+    var tvShows: [TvShow]? //{
+////        didSet {
+////            collectionView?.reloadData()
+////        }
+//    }
     
     var defaultToMovies: Bool = true
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         setupCollectionView()
        
+        collectionView.reloadData()
     }
-    
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        setupCollectionViewFlowLayout()
-    }
-    
+
     private func setupCollectionView() {
         collectionView.dataSource = self
         collectionView.delegate = self
-    }
-
-    
-    private func setupCollectionViewFlowLayout() {
-
-        if collectionViewLayout == nil {
-
-            let numberOfItemsPerRow: CGFloat = 2
-            let lineSpacing: CGFloat = 10
-            let interItemSpacing: CGFloat = 5
-
-            let width = (collectionView.frame.width - (numberOfItemsPerRow - 1) * interItemSpacing) / numberOfItemsPerRow
-
-            let height = width
-
-            collectionViewLayout = UICollectionViewFlowLayout()
-
-            collectionViewLayout.itemSize = CGSize(width: width, height: height)
-
-            collectionViewLayout.sectionInset = UIEdgeInsets.zero
-            collectionViewLayout.scrollDirection = .vertical
-            collectionViewLayout.minimumLineSpacing = lineSpacing
-            collectionViewLayout.minimumInteritemSpacing = interItemSpacing
-
-            collectionView.setCollectionViewLayout(collectionViewLayout, animated: true)
-
-        }
-
+        
+        collectionView.contentInsetAdjustmentBehavior = .never
     }
     
+    override func viewWillLayoutSubviews() {
+        
+        
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+        super.viewWillLayoutSubviews()
 
+//        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+//        layout.minimumInteritemSpacing = 0
+//        layout.minimumLineSpacing = 0
+//
+//        let bounds = collectionView.bounds
+//
+//
+//
+//        layout.itemSize = CGSize(width: (bounds.width - 20)/2, height: (bounds.height - 5)/4)
+        
+        //collectionView.reloadData()
+        
+    }
 }
 
 
 
 
-extension MoviesAndTVShowsViewController : UICollectionViewDataSource, UICollectionViewDelegate{
+extension MoviesAndTVShowsViewController : UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
         
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//                if let movies = self.movies {
+//                    return movies.count
+//                 }
+//
+//                if let tvshows = self.tvShows {
+//                           return tvshows.count
+//                  }
+//
+                return 1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 
          if let movies = self.movies {
             return movies.count
          }
-        
+
         if let tvshows = self.tvShows {
                    return tvshows.count
                 }
-            
+
         return 1
+        
+        //return
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -104,20 +107,20 @@ extension MoviesAndTVShowsViewController : UICollectionViewDataSource, UICollect
         let placeholder = UIImage(named: "placeholder")
             
         cell.placeholderImage = placeholder
-                
+        cell.imageView.image = placeholder
         
-        //cell.contentView.layer.cornerRadius = 10.0;
-//       cell.contentView.layer.borderWidth = 1.0;
-//       cell.contentView.layer.borderColor = UIColor.clear.cgColor;
-//       cell.contentView.layer.masksToBounds = true;
-//       
-//       cell.layer.shadowColor = UIColor.lightGray.cgColor
-//       cell.layer.shadowOffset = CGSize(width: CGFloat(0), height: CGFloat(2));
-//       cell.layer.shadowRadius = 2.0;
-//       cell.layer.shadowOpacity = 1.0;
-//       cell.layer.masksToBounds = false;
-//       cell.contentView.clipsToBounds = true
-//       cell.layer.shadowPath = UIBezierPath(roundedRect:cell.bounds, cornerRadius:cell.contentView.layer.cornerRadius).cgPath;
+        cell.contentView.layer.cornerRadius = 10.0;
+       cell.contentView.layer.borderWidth = 1.0;
+       cell.contentView.layer.borderColor = UIColor.clear.cgColor;
+       cell.contentView.layer.masksToBounds = true;
+       
+       cell.layer.shadowColor = UIColor.lightGray.cgColor
+       cell.layer.shadowOffset = CGSize(width: CGFloat(0), height: CGFloat(2));
+       cell.layer.shadowRadius = 2.0;
+       cell.layer.shadowOpacity = 1.0;
+       cell.layer.masksToBounds = false;
+       cell.contentView.clipsToBounds = true
+       cell.layer.shadowPath = UIBezierPath(roundedRect:cell.bounds, cornerRadius:cell.contentView.layer.cornerRadius).cgPath;
         
         if let movies = self.movies {
             cell.imageView.image = placeholder
@@ -130,13 +133,46 @@ extension MoviesAndTVShowsViewController : UICollectionViewDataSource, UICollect
             return cell
         }
 
-        cell.imageView.image = placeholder
+        
         cell.movie = nil
                
+        //cell.layoutIfNeeded()
+        
+        
+        
         return cell
     }
     
-        
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+    }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let bounds = collectionView.bounds
+
+        return CGSize(width: (floor(bounds.width)-100)/2, height: (floor(bounds.height)-50)/3)
+
+    }
     
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        //let itemsPerRow:CGFloat = 4
+//        let hardCodedPadding:CGFloat = 5
+//        let itemHeight = collectionView.bounds.height - (2 * hardCodedPadding)
+//        let itemWidth = itemHeight * 300 / 444
+//        //print("FeaturedCategoryCell \(itemWidth):\(itemHeight)")
+//        return CGSize(width: itemWidth, height: itemHeight)
+//
+//
+//    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
 }
+
+
--- TheBigScreenDb/View/Featured/VideoCell.swift
+++ TheBigScreenDb/View/Featured/VideoCell.swift
@@ -15,30 +15,20 @@ class VideoCell : UICollectionViewCell {
     
     
     @IBOutlet weak var imageView: UIImageView!
-    //@IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     var placeholderImage: UIImage?
       
-    var title:String?
+    
     
     var movie:Movie? = nil {
         didSet {
             if let movie = movie,
                 let url = movie.poster_path {
-                //let placeholderImage = UIImage(named: "placeholder")!
-                title = movie.title
-
-                //let filter = AspectScaledToFillSizeFilter(size: CGSize(width: 100, height: 150))
-                imageView.af.setImage(withURL: url, placeholderImage: placeholderImage) //, filter: filter)
-            } else {
-                title = nil                
+                    downloadImage(with: url)
+                
+                    titleLabel?.text = movie.title
             }
-            
-            
-//            if let movie = movie {
-//                titleLabel.text = movie.title
-//            }
-            
         }
     }
     
@@ -46,40 +36,19 @@ class VideoCell : UICollectionViewCell {
             didSet {
                 if let tvShow = tvShow,
                     let url = tvShow.poster_path {
-                    //let placeholderImage = UIImage(named: "placeholder")!
-                    title = tvShow.name
+                    downloadImage(with: url)
+                    
+                    titleLabel?.text = tvShow.name
 
-                    //let filter = AspectScaledToFillSizeFilter(size: CGSize(width: 100, height: 150))
-                    imageView.af.setImage(withURL: url, placeholderImage: placeholderImage) //, filter: filter)
-                } else {
-                    title = nil
                 }
-                
-                
-    //            if let movie = movie {
-    //                titleLabel.text = movie.title
-    //            }
-                
             }
         }
     
-    override func awakeFromNib() {
-           super.awakeFromNib()
-           // Initialization code
-       }
-
-    func run() {
-        
-        if let movie = movie,
-            let url = movie.poster_path {
-            //let placeholderImage = UIImage(named: "placeholder")!
-            title = movie.title
-
-            //let filter = AspectScaledToFillSizeFilter(size: CGSize(width: 100, height: 150))
-            imageView.af.setImage(withURL: url, placeholderImage: placeholderImage) //, filter: filter)
-        } else {
-            title = nil
-        }
+    func downloadImage(with url: URL) {
+     
+        imageView.af.setImage(withURL: url, placeholderImage: placeholderImage)
     }
+    
+    
     
 }
